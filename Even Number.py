c00547931bc9520fe71ad865c3a30949d2b22e27
Muhammad Mukarram Asad <mukarramasad89@gmail.com>
2022-01-02 21:58:43 +0500

    All files are push to the git.

---
+++ Even Number.py
@@ -0,0 +1,7 @@
+print(" A program to check that a given number is Odd or Even ")
+number = int(input("Enter a number here: "))
+if(number%2==0):
+    print("Number is even.")
+else:
+    print("Number is not even. May be it is something else.")
+    
